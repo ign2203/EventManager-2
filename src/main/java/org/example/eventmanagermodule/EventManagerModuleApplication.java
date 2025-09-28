47fa22a1e8714ace5ac112906a66bf3f20d4e344
ign2203 <ign2203@yandex.ru>
2025-09-28 11:19:43 +0400

    commit first

---
+++ src/main/java/org/example/eventmanagermodule/EventManagerModuleApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventmanagermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eventmanagermodule/EventManagerModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventmanagermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
